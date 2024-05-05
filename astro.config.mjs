76f52a7649ea270c6c943f2c26d3663b5d29f22d
Brando Vidal <yisusdracon@gmail.com>
2024-05-05 17:53:37 -0500

    chore: implement Home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
-import auth from 'auth-astro';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import node from '@astrojs/node'
+import auth from 'auth-astro'
 
-import db from "@astrojs/db";
+import db from '@astrojs/db'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  }), auth(), db()],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false
+    }),
+    auth(),
+    db()
+  ],
   output: 'server',
   adapter: node({
     mode: 'standalone'
   })
-});
+})
---
+++ src/components/UserButton.tsx
@@ -0,0 +1,59 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuItem
+} from '@/ui/dropdown-menu'
+import { Button } from '@/ui/button'
+
+import { UserIcon } from '@/icons/UserIcon'
+
+import type { Session } from '@auth/core/types'
+const { signIn, signOut } = await import('auth-astro/client')
+
+interface Props {
+  session: Session | null
+}
+
+export function UserButton ({ session }: Props) {
+  const login = () => {
+    signIn('google')
+  }
+  const logout = () => {
+    signOut()
+  }
+
+  return session !== null && session.user ? (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          className='text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50'
+          size='icon'
+          variant='ghost'
+        >
+          <UserIcon className='h-5 w-5' />
+          <span className='sr-only'>Toggle user menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        <DropdownMenuLabel className='font-medium text-md'>
+          {session.user.name}
+        </DropdownMenuLabel>
+        <DropdownMenuLabel className='font-normal'>
+          {session.user.email}
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={logout}>
+          <a href='#'>Logout</a>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  ) : (
+    <Button onClick={login}>
+      <UserIcon className='h-5 w-5' />
+      Login
+    </Button>
+  )
+}
