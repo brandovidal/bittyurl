8582c1d1e56920d927c7871a7e8056a89bad8fb4
Brando Vidal <yisusdracon@gmail.com>
2024-05-05 02:17:44 -0500

    chore: login and logout with google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import node from '@astrojs/node'
 
-import node from "@astrojs/node";
+import auth from 'auth-astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  })],
-  output: "server",
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false
+    }),
+    auth()
+  ],
+  output: 'server',
   adapter: node({
-    mode: "standalone"
+    mode: 'standalone'
   })
-});
+})
