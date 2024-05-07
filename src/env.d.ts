84675e495c8306c3103ec88709d1cde73f2f46f7
Brando Vidal <yisusdracon@gmail.com>
2024-05-07 07:12:06 -0500

    chore: deploy app

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,13 @@
 /// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly DB_PASSWORD: string;
+  readonly PUBLIC_POKEAPI: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
