79e7bce01f4ba8ac4d69dbaac37f487d2dcc949c
Brando Vidal <yisusdracon@gmail.com>
2024-05-07 05:33:00 -0500

    chore: remove import not used

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config'
-
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import auth from 'auth-astro'
 import db from '@astrojs/db'
-
 import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
