4600f627e116c418e4a10d2ee51938b324a95b30
Brando Vidal <yisusdracon@gmail.com>
2024-05-07 21:15:45 -0500

    chore: use exact node package by auth astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,16 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
-import auth from 'auth-astro'
-import db from '@astrojs/db'
-import cloudflare from '@astrojs/cloudflare'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import auth from 'auth-astro';
+import db from '@astrojs/db';
+import cloudflare from '@astrojs/cloudflare';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false
-    }),
-    auth(),
-    db()
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), auth(), db()],
   output: 'server',
   adapter: cloudflare(),
   vite: {
@@ -22,7 +18,7 @@ export default defineConfig({
       external: ['node:path', 'node:process']
     },
     build: {
-      minify: false,
+      minify: false
     }
   }
-})
+});
