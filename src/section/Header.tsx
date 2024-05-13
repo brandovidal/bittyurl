eaa474f1517e4506fb069a290b43970e44844c3f
Brando Vidal <yisusdracon@gmail.com>
2024-05-13 18:25:39 -0500

    fix: set user button in header section

--- src/section/Header.tsx
+++ src/section/Header.tsx
@@ -1,44 +1,18 @@
 import { Button } from '@/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '@/ui/dropdown-menu'
-import { Link } from '@/ui/link'
 import { Sheet, SheetContent, SheetTrigger } from '@/ui/sheet'
 
-import { CircleUser, Menu, Package2 } from 'lucide-react'
+import { Menu } from 'lucide-react'
 
-export function UserMenu () {
-  return (
-    <>
-      <Link
-        href='#'
-        className='flex items-center gap-2 text-lg font-semibold md:text-base'
-      >
-        <Package2 className='h-6 w-6' />
-        <span className='sr-only'>Bittylink</span>
-      </Link>
-      <Link
-        href='#'
-        className='text-foreground transition-colors hover:text-foreground'
-      >
-        Dashboard
-      </Link>
-      <Link
-        href='#'
-        className='text-muted-foreground transition-colors hover:text-foreground'
-      >
-        Analytics
-      </Link>
-    </>
-  )
+import type { Session } from '@auth/core/types'
+
+import { UserButton } from '@/components/user/UserButton'
+import { UserMenu } from '@/components/user/UserMenu'
+
+interface Props {
+  session: Session | null
 }
 
-export function Header () {
+export function Header ({ session }: Props) {
   return (
     <header className='sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6'>
       <nav className='hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6'>
@@ -58,22 +32,7 @@ export function Header () {
         </SheetContent>
       </Sheet>
       <div className='flex w-full items-center justify-end  gap-4 md:ml-auto md:gap-2 lg:gap-4'>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant='secondary' size='icon' className='rounded-full'>
-              <CircleUser className='h-5 w-5' />
-              <span className='sr-only'>Toggle user menu</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align='end'>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-            <DropdownMenuItem>Support</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Logout</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <UserButton session={session} />
       </div>
     </header>
   )
