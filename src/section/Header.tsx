0c8321ea5d9d5c801254714c5483270537e86356
Brando Vidal <yisusdracon@gmail.com>
2024-05-14 03:04:20 -0500

    chore: setting pages and sections

--- src/section/Header.tsx
+++ src/section/Header.tsx
@@ -15,26 +15,32 @@ interface Props {
 
 export function Header ({ session }: Props) {
   return (
-    <header className='sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6'>
-      <nav className='hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6'>
-        <UserMenu />
-      </nav>
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant='outline' size='icon' className='shrink-0 md:hidden'>
-            <Menu className='h-5 w-5' />
-            <span className='sr-only'>Toggle navigation menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side='left'>
-          <nav className='grid gap-6 text-lg font-medium'>
-            <UserMenu />
-          </nav>
-        </SheetContent>
-      </Sheet>
-      <div className='flex w-full items-center justify-end  gap-4 md:ml-auto md:gap-2 lg:gap-4'>
-        <ModeToggle />
-        <UserButton session={session} />
+    <header className='sticky top-0 flex h-16 items-center justify-center gap-4 border-b px-4 md:px-6'>
+      <div className='flex items-center w-full max-w-7xl'>
+        <nav className='hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6'>
+          <UserMenu />
+        </nav>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant='outline'
+              size='icon'
+              className='shrink-0 md:hidden'
+            >
+              <Menu className='h-5 w-5' />
+              <span className='sr-only'>Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side='left'>
+            <nav className='grid gap-6 text-lg font-medium'>
+              <UserMenu />
+            </nav>
+          </SheetContent>
+        </Sheet>
+        <div className='flex w-full items-center justify-end  gap-4 md:ml-auto md:gap-2 lg:gap-4'>
+          <ModeToggle />
+          <UserButton session={session} />
+        </div>
       </div>
     </header>
   )
